b0cdc879863c6252bdfeebfbfb44f6b33f7f90cd
Schahriar SaffarShargh <info@schahriar.com>
2017-01-21 20:18:00 -0800

    Browser initialize as a BDD style unit and better sync wrapper

--- bin/globals.js
+++ bin/globals.js
@@ -4,11 +4,28 @@ const sync = require('synchronize');
 /** @todo: add browser support using argv */
 global.browser = new have.Browser();
 global.chai = require("chai");
-global.chaiAsPromised = require("chai-as-promised");
-// enables chai assertion chaining
-chaiAsPromised.transferPromiseness = browser.getWebDriver().transferPromiseness;
 
-chai.use(chaiAsPromised);
+/** @todo: re-add if promises are shipped by default */
+// global.chaiAsPromised = require("chai-as-promised");
+// enables chai assertion chaining
+//chaiAsPromised.transferPromiseness = browser.getWebDriver().transferPromiseness;
+// chai.use(chaiAsPromised);
+
 chai.should();
 
-global.sync = sync.asyncIt;
+global.sync = (func) => {
+  return (done) => {
+    sync.fiber(() => {
+      let syncFunc = sync.asyncIt(func);
+      sync.await(syncFunc(done, sync.defer()));
+      done();
+    });
+  };
+};
+
+/** @todo: Add TDD & QUnit options for Initial and final test */
+// Initial test
+it("should start test suite", function (done) {
+  this.timeout(10000);
+  global.browser.on('ready', done);
+});
